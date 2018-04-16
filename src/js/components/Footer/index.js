1fba0a7a68724fe525f326d897a4b936bdf981a4
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-04-16 21:46:21 +0300

    task2

--- src/js/components/Footer/index.js
+++ src/js/components/Footer/index.js
@@ -5,15 +5,15 @@ export default class Footer extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      socials: [{name: 'fb'}, {name: 'tw'}, {name: 'ln'}]
-    }
+      socials: [{ name: 'fb' }, { name: 'tw' }, { name: 'ln' }],
+    };
   }
 
   render() {
     return (
       <footer className='footer app__footer'>
-          footer
-          <Socials socials={this.state.socials} />
+        footer
+        <Socials socials={this.state.socials} />
       </footer>
     );
   }
