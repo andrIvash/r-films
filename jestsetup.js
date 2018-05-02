7da901167ca8f55923b4599997913f28c28bd557
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-05-02 22:37:39 +0300

    first test

--- jestsetup.js
+++ jestsetup.js
@@ -1,4 +1,8 @@
-import { shallow, render, mount } from 'enzyme';
+import { shallow, render, mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
--- src/js/components/ContentInfo/contentInfo.test.js
+++ src/js/components/ContentInfo/contentInfo.test.js
@@ -5,4 +5,30 @@ describe('ContentInfo', () => {
   it('should be defined', () => {
     expect(ContentInfo).toBeDefined();
   });
+  it('should render correctly', () => {
+    const wrapper = shallow(
+      <ContentInfo
+        films={[]}
+        onFilmSelect={() => {}}
+      /> );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render child elements', () => {
+    const arr = [{ id: 1 }, { id: 2 }, { id: 3 }];
+    const wrapper = shallow(
+      <ContentInfo
+        films={arr}
+        onFilmSelect={() => {}}
+      /> );
+    expect(wrapper.find('.film-list').children().length).toEqual(arr.length);
+  });
+  it('should render warning title if no data passed', () => {
+    const wrapper = shallow(
+      <ContentInfo
+        films={[]}
+        onFilmSelect={() => {}}
+      /> );
+    expect(wrapper.find('.film-list').children().length).toEqual(1);
+    expect(wrapper.find('.no-data').text()).toBe('No films found');
+  });
 });
