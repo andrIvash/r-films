999ecebc3a075edd889c218cda0115ff6b4ee9be
Andriy Ivashchenko <andr.ivas12@gmail.com>
2018-05-03 22:06:10 +0300

    test filItem

---
+++ src/js/components/FilmItem/filmItem.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import FilmItem from './index';
+
+const props = {
+  film: {
+    poster_path: 'http://test',
+    release_date: '1924',
+    title: 'title',
+    genres: ['drama'],
+  },
+  id: 22,
+  onFilmSelect: jest.fn(),
+};
+
+describe('FilmItem', () => {
+  it('should be defined', () => {
+    expect(FilmItem).toBeDefined();
+  });
+  it('should render correctly', () => {
+    const wrapper = shallow(
+      <FilmItem {...props} /> );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should render write values ', () => {
+    const wrapper = shallow(
+      <FilmItem {...props} /> );
+    expect(wrapper.find('.film-item__img')
+      .getElement().props.src).toBe('http://test');
+    expect(wrapper.find('.film-item__title').text()).toBe('title');
+    expect(wrapper.find('.film-item__year').text()).toBe('1924');
+  });
+
+  it(`should calls onFilmSelect with an argument 
+    when FilmItem is clicked`, () => {
+    const wrapper = shallow(
+      <FilmItem {...props} /> );
+    wrapper.simulate('click');
+    expect(props.onFilmSelect).toHaveBeenCalledTimes(1);
+    expect(props.onFilmSelect).toHaveBeenCalledWith(22);
+  });
+});
