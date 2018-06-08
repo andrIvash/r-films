d5319b35a362f5e5e08ed0ff54f2754f0149d8d0
andrIvash <andr.ivas12@gmail.com>
2018-06-08 17:29:09 +0300

    merge

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 // import createHistory from 'history/createBrowserHistory';
 import { Route, Switch } from 'react-router';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 // import { ConnectedRouter, routerReducer,
 //  routerMiddleware, push } from 'react-router-redux';
-=======
-import { ConnectedRouter } from 'react-router-redux';
->>>>>>> task6
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 import '../styles/app.scss';
